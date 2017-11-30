10148fcf54af45b965c0bb571f6e513471a90f97
Rob Byrne <rob@leanrob.com>
2017-11-30 02:00:32 -0800

    Complete Overhaul

--- portfolio/content/post/hello-world.md
+++
@@ -1,8 +0,0 @@
-+++
-date = "2017-05-01T22:28:22-07:00"
-draft = false
-title = "hello world"
-
-+++
-
-This is some good shit right here. Hello world
---
+++ src/components/PortfolioPage/StartupAcceleratorSection/StartupAcceleratorSection.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import './startupAcceleratorSection.css'
+
+const StartupAcceleratorSection = () => {
+	return (
+		<div className="main-content" id="startupAccelerator">
+			<h1>Accelerator</h1>
+			<p>End of section</p>
+		</div>
+	)
+};
+
+export default StartupAcceleratorSection
